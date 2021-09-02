b51b93df47c94462f8cc7e74fff99781a888014f
GeraldiWinson <s16081801@student.ubaya.ac.id>
2021-09-02 13:14:40 +0700

    0.1
    Project Started

---
+++ app/src/main/java/com/multimedia2018/ubaya/a160818031_advweek4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.multimedia2018.ubaya.a160818031_advweek4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
