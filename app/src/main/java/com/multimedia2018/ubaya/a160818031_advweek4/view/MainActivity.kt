714267132d22267816b2c898d6b065fd890292e5
GeraldiWinson <s16081801@student.ubaya.ac.id>
2021-09-02 14:37:45 +0700

    1.0
    Class Example Finished
    
    Added:
    RecyclerView -> need improvements, possibly worked on Homework / next week
    FragmentList for student (detail, list)

--- app/src/main/java/com/multimedia2018/ubaya/a160818031_advweek4/view/MainActivity.kt
+++ app/src/main/java/com/multimedia2018/ubaya/a160818031_advweek4/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.multimedia2018.ubaya.a160818031_advweek4
+package com.multimedia2018.ubaya.a160818031_advweek4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.multimedia2018.ubaya.a160818031_advweek4.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
