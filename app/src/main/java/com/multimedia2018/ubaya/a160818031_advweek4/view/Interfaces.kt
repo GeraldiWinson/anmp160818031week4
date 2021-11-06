05e76876b2342823a730d9dd1a6aabc40cae4481
GeraldiWinson <s16081801@student.ubaya.ac.id>
2021-11-06 10:36:40 +0700

    6.9
    Homework Week 11 Finished (With minor bug)
    
    Added:
    Penerapan DataBinding pada Student Detail
    
    Bug:
    Unknown NullPointerException error di image StudentDetailFragment.kt, padahal penerapan sama persis seperti di PPT dan sudah berjalan normal di StudentListItem?

--- app/src/main/java/com/multimedia2018/ubaya/a160818031_advweek4/view/Interfaces.kt
+++ app/src/main/java/com/multimedia2018/ubaya/a160818031_advweek4/view/Interfaces.kt
@@ -1,7 +1,17 @@
 package com.multimedia2018.ubaya.a160818031_advweek4.view
 
 import android.view.View
+import com.multimedia2018.ubaya.a160818031_advweek4.model.Student
 
 interface ButtonDetailClickListener {
     fun onButtonDetailClick(v: View)
 }
+
+interface SDButtonUpdateClickListener {
+    fun onSDButtonUpdateClick(v: View)
+}
+
+interface SDButtonNotificationClickListener{
+    fun onSDButtonNotificationClick(v: View, obj: Student)
+}
+
--- app/src/main/java/com/multimedia2018/ubaya/a160818031_advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/multimedia2018/ubaya/a160818031_advweek4/view/StudentDetailFragment.kt
@@ -6,9 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.multimedia2018.ubaya.a160818031_advweek4.R
+import com.multimedia2018.ubaya.a160818031_advweek4.databinding.FragmentStudentDetailBinding
+import com.multimedia2018.ubaya.a160818031_advweek4.model.Student
 import com.multimedia2018.ubaya.a160818031_advweek4.util.loadImage
 import com.multimedia2018.ubaya.a160818031_advweek4.viewmodel.DetailViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
@@ -17,15 +21,20 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_student_detail.*
 import java.util.concurrent.TimeUnit
 
-class StudentDetailFragment : Fragment() {
+class StudentDetailFragment : Fragment(), SDButtonNotificationClickListener, SDButtonUpdateClickListener {
     private lateinit var viewModel:DetailViewModel
+    private lateinit var dataBinding:FragmentStudentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+        //Week 4 old
+        //return inflater.inflate(R.layout.fragment_student_detail, container, false)
+
+        dataBinding = DataBindingUtil.inflate<FragmentStudentDetailBinding>(inflater, R.layout.fragment_student_detail, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -40,6 +49,11 @@ class StudentDetailFragment : Fragment() {
 
     fun observeDetailViewModel() {
         viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            dataBinding.student = it
+            dataBinding.notifListener = this
+            dataBinding.updateListener = this
+
+        /* Week 4 old
             txtId.setText(viewModel.studentLD.value?.id)
             txtName.setText(viewModel.studentLD.value?.name)
             txtBod.setText(viewModel.studentLD.value?.bod)
@@ -52,12 +66,27 @@ class StudentDetailFragment : Fragment() {
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe {
-                        Log.d("Messages", "five seconds")
                         MainActivity.showNotification(student.name.toString(),
                             "A new notification created",
                             R.drawable.ic_baseline_person_24)
                     }
             }
+            */
         })
+    }
+
+    override fun onSDButtonUpdateClick(v: View) {
+        //Transisi ke halaman sebelumnya
+    }
+
+    override fun onSDButtonNotificationClick(v: View, obj: Student) {
+        Observable.timer(5, TimeUnit.SECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                MainActivity.showNotification(dataBinding.student?.name.toString(),
+                    "A new notification created",
+                    R.drawable.ic_baseline_person_24)
+            }
     }
 }
